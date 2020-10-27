8fedf43598db03478fb752b154c1162f35d8a50c
Joseph Beeson <joseph.beeson@dekra.com>
2020-10-27 16:19:07 +0100

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "name" {
   description = "Name of the resource group."
-  value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.outputs.name : data.azurerm_resource_group.this.name
+  value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.0.outputs.name : data.azurerm_resource_group.this.0.name
 }
 
 output "id" {
   description = "Id of the resource group."
-    value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.outputs.id : data.azurerm_resource_group.this.id
+    value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.0.outputs.id : data.azurerm_resource_group.this.0.id
 }
 
 output "tags" {
   description = "Tags of the resource group."
-    value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.outputs.tags : data.azurerm_resource_group.this.tags
+    value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.0.outputs.tags : data.azurerm_resource_group.this.0.tags
 }
