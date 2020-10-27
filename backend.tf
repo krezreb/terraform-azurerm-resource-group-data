ecfc217df45f104607ccc099d73fa75fcf54148f
Joseph Beeson <joseph.beeson@dekra.com>
2020-10-27 16:12:30 +0100

    bump azurerm version

--- backend.tf
+++ backend.tf
@@ -1,8 +1,13 @@
 terraform {
   required_version = ">= 0.12.0"
   required_providers {
-    azurerm = "~> 1.44"
+    azurerm = "~> 2.33.0"
   }
-
   backend "local" {}
+}
+
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "~> 2.33.0"
+  features {}
 }
--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,6 @@ output "id" {
     value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.outputs.id : data.azurerm_resource_group.this.id
 }
 
-
 output "tags" {
   description = "Tags of the resource group."
     value       = var.rspath_resource_group != "" ? data.terraform_remote_state.resource_group.outputs.tags : data.azurerm_resource_group.this.tags
